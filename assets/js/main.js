ad94da566772603f356988448b34da7b50c0b167
karinlee <karinlee92@gmail.com>
2023-01-23 16:45:33 +0100

    initial

---
+++ assets/js/main.js
@@ -0,0 +1,23 @@
+const headline = document.querySelector("h1");
+
+const body = document.querySelector("body");
+
+function changeOne(){
+    headline.style.color = "#8DCBE6";
+    body.style.backgroundColor ="#FFEA20";
+}
+
+function changeTwo(){
+    headline.style.color = "#2B3467";
+    body.style.backgroundColor ="#FCFFE7";
+}
+
+function changeThree(){
+    headline.style.color = "#D61C4E";
+    body.style.backgroundColor ="#FEDB39";
+}
+
+function changeFour(){
+    headline.style.color = "#CDE990";
+    body.style.backgroundColor ="#FFD4D4";
+}
